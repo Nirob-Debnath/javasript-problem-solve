bbb9402832b565ca5039e9713509c223400399b9
unknown <nirobdebnath456@gmail.com>
2025-08-18 00:28:26 +0600

    Problem 01 Solved

---
+++ problem_01_reverse_a_string.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+  let reversed = "";                  // Start with an empty string to store the reversed text
+  for (let i = str.length - 1; i >= 0; i--) { // Loop backwards from the last character
+    reversed += str[i];               // Add each character to the new string
+  }
+  return reversed;                    // Return the reversed string
+}
+
+console.log(reverseString("hello")); // "olleh"
